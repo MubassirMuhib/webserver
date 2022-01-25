5b3e4f47e6cd666fc49482ca5553198caaa769fa
MubassirMuhib <BlackClover1003@gmail.com>
2022-01-25 09:00:54 +0600

    demo web server

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import csv
+
 app = Flask(__name__)
 
 
@@ -11,3 +12,30 @@ def default_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+
+def file_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        name = data["name"]
+        email = data["email"]
+        message = data["message"]
+
+        # you just have to memorize this next two lines
+        csv_writer = csv.writer(database, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+# we have to write action= "submit_form" in action section in the form area which is in the html file
+# also method="post" must be added in the form beside action also in the html file
+# 'GET' means that the browser wants us to sent information. 'POST' means browser wants us to save information
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            file_to_csv(data)
+            return redirect('/message_accepted.html')
+        except:
+            return 'Did not save to the database'
+    else:
+        return 'Something went wrong. Try again'
